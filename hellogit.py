e0f9e0095fbc2c36a7bb966b4a1c121b249c1de8
Hecper <hecper18@hotmail.com>
2024-10-30 13:56:46 -0700

    Hello GitHub v2

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,4 @@
 print ("hello gitHub!")
 print ("Este Print es despues de haber subido el repo a GitHub")
+print ("Este fue despues todavia del commit desde GitHub cuando cree el README")
 print ("que tal el curso?")
